9eb28e403158ebdbaba9cc3ba382bea56726bb75
DanielDerefaka <bryancagedx@gmail.com>
2024-01-27 23:49:11 +0100

    thme provider

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, { createContext, useContext, useState, useEffect } from 'react'
+
+
+interface ThemeContextType {
+    mode: string;
+    setmode:  (mode : string ) => void ;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined  >(undefined);
+
+
+
+export function ThemeProvider({children}: {children : React.ReactNode}) {
+    const [mode, setmode] = useState('');
+
+    const handleThemeChange = () => {
+        if(mode === 'dark'){
+            setmode('light');
+            document.documentElement.classList.add('light');
+        }else {
+            setmode('dark');
+            document.documentElement.classList.add('dark');
+        }
+    }
+
+
+    useEffect(() => {
+        handleThemeChange();
+
+        
+    }, [mode])
+
+    return (
+        <ThemeContext.Provider value={{mode, setmode}}>
+{children}
+     </ThemeContext.Provider>
+    )
+}
+
+
+export function useTheme() {
+    const context =  useContext(ThemeContext);
+
+    if(context === undefined) {
+        throw new Error("Use Theme Provider");
+    }
+
+    return context;
+}
